eba72bd530247a8f4e589cdd827e1bad07ea9de6
mdasad7827 <mohaammadasad1996@gmail.com>
2020-12-16 23:06:21 +0530

    added helper function

--- src/index.js
+++ src/index.js
@@ -13,10 +13,18 @@ app.get("/newFeeds", async (req, res) => {
   res.send(
     await newsArticleModel
       .find()
-      .skip(Number(req.query.offset || 0))
-      .limit(Number(req.query.limit || 10))
+      .skip(filter(req.query.offset, 0))
+      .limit(filter(req.query.limit, 10))
   );
 });
+
+const filter = (value, defVal) => {
+  if (value === null || value === undefined || isNaN(Number(value))) {
+    return defVal;
+  } else {
+    return Number(value);
+  }
+};
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
