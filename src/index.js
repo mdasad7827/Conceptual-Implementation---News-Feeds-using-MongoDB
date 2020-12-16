e4f4a7a66a4f340dbcf72c8f76421bd558b66408
mdasad7827 <mohaammadasad1996@gmail.com>
2020-12-16 22:02:45 +0530

    oneline

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ const app = express();
 const port = 8080;
 
 const onePageArticleCount = 10;
-const {newsArticleModel} = require("./connector");
+const { newsArticleModel } = require("./connector");
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/newFeeds", async (req, res) => {
-  const { limit, offset } = req.query;
-
-  const lim = limit == undefined ? 10 : isNaN(limit) ? 10 : Number(limit);
-  const off = offset == undefined ? 0 : isNaN(offset) ? 0 : Number(offset);
-  const result = await newsArticleModel.find().skip(off).limit(lim);
-  res.send(result);
+  res.send(
+    await newsArticleModel
+      .find()
+      .skip(Number(req.query.offset || 0))
+      .limit(Number(req.query.limit || 10))
+  );
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
